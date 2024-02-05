7c942773c0a249898c3789ae704ac7414ca78fdc
DESKTOP-24V2TD5\Martiny <mawinClone@gmail.com>
2024-02-05 17:11:42 +0700

    position test

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="App">
+        <h1><p>home page</p></h1>
+        <h1><p>home page</p></h1>
+        
+      </div>
+
+      <div className="Box">
+        <div className="Child-Box"><p>live</p></div>
+      </div>
+      <i class="fa fa-shopping-cart"></i>
     </div>
   );
 }
